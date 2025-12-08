640a3c6950d431632440791552495a66f15e5f60
Anexy <113385401+Never-More-1@users.noreply.github.com>
2025-12-09 00:54:08 +0300

    Add files via upload

---
+++ DZ_41/src/main/java/springApp/service/UserService.java
@@ -0,0 +1,24 @@
+package springApp.service;
+
+import org.springframework.stereotype.Service;
+import springApp.model.User;
+import springApp.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.getUserById(id);
+    }
+}
